88bc0386edbcaee1e60d5346e1fd77cb4ceb80e9
MateusSilva00 <mateus.2020@alunos.utfpr.edu.br>
2021-08-02 15:09:09 -0300

    Update functions

---
+++ Functions/longData.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Size of int = %ld\n", sizeof(int));
+    printf("Size of long = %ld\n", sizeof(long));
+    printf("Size of long long = %ld", sizeof(long long));
+}
